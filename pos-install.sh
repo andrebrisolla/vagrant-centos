6b5c846fdbd78ebae652b14946949b035900168f
André Brisolla <andremuzel@gmail.com>
2021-02-23 16:36:16 -0300

    Create pos-install.sh

---
+++ pos-install.sh
@@ -0,0 +1,11 @@
+# Install basics
+yum install vim zip unzip telnet wget net-tools -y
+
+# Install Salt
+yum install https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm -y
+yum install salt-minion -y
+
+# Config Salt
+echo "lab-$(hostname)" > /etc/salt/minion_id
+echo "192.168.0.111 salt" >> /etc/hosts
+systemctl restart salt-minion
